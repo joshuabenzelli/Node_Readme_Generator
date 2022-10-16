9f1e869cee4ff3c567264a6c5a0f1a192ac8a35a
Joshua Booth <joshuabenzelli@gmail.com>
2022-10-16 12:28:14 -0600

    Got readme functionality working

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
-# Node_Readme_Generator
+
+# README Generated for jklhsdf!
+
+## Table of contents:
+    * 1:[Description](Description)
+
+    * 2:[Github](Github)
+
+    * 3:[Email](Email)
+
+## Description:
+    sdfsdfg
+
+## Github: [sdfgsdg](https://github.com/sdfgsdg)
+  
+### Feel free to email me with any questions or concerns at sdgsdgs!
+  
+  
--- index.js
+++ index.js
@@ -1,13 +1,57 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-// TODO: Create an array of questions for user input
-const questions = [];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "name",
+      message: "What is the name of your project?",
+    },
+    {
+      type: "input",
+      name: "description",
+      message: "What is a brief description of your project?",
+    },
+    {
+      type: "input",
+      name: "github",
+      message: "What is your github username?",
+    },
+    {
+      type: "input",
+      name: "email",
+      message: "What is your email?",
+    },
+  ]);
+};
 
-// TODO: Create a function to initialize app
-function init() {}
+const generateReadme = ({ name, description, github, email }) =>
+  `
+# README Generated for ${name}!
 
-// Function call to initialize app
+## Table of contents:
+    * 1: Description
+
+    * 2: Github
+
+    * 3: Email
+
+## Description:
+    ${description}
+
+## Github: [${github}](https://github.com/${github})
+  
+### Feel free to email me with any questions or concerns at ${email}!
+  
+  `;
+
+const init = () => {
+  promptUser()
+    .then((answers) => fs.writeFileSync("README.md", generateReadme(answers)))
+    .then(() => console.log("Successfully wrote to README.md"))
+    .catch((err) => console.error(err));
+};
+
 init();
